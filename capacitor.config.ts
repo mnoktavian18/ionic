8ae1c955c2d326ecc14b5f25d0e69d00dafa9d1f
mnoktavian18 <muhammad.oktavian@gramediaprinting.com>
2023-01-02 14:29:14 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-framework',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
